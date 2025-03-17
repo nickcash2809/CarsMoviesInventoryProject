10be1413c968ae70183efc453ed4598d5c988480
AndresMiranda <61157532+afmirandad@users.noreply.github.com>
2025-03-17 17:32:14 +0000

    DB connection properties updated

--- src/main/java/com/carsmoviesinventory/app/Entities/CarsMoviesEntity.java
+++ src/main/java/com/carsmoviesinventory/app/Entities/CarsMoviesEntity.java
@@ -1,5 +1,7 @@
 package com.carsmoviesinventory.app.Entities;
 import java.util.UUID;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
 
 import lombok.*;
 
@@ -7,8 +9,11 @@ import lombok.*;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Table(name="CarsMovies")
 public class CarsMoviesEntity{
 
+    @Id
     private UUID id;
     private String carMovieName;
     private String carMovieYear;
