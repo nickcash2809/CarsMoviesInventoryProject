d285256b63f1fdee8a893ba78a645aaedf75045a
AndresMiranda <61157532+afmirandad@users.noreply.github.com>
2025-03-17 18:43:06 +0000

    SQL file creation for initial data and DB config parameters update

--- src/main/java/com/carsmoviesinventory/app/Services/CarsMoviesService.java
+++ src/main/java/com/carsmoviesinventory/app/Services/CarsMoviesService.java
@@ -1,1 +1,19 @@
 package com.carsmoviesinventory.app.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carsmoviesinventory.app.Repositories.CarsMoviesRepository;
+import com.carsmoviesinventory.app.Entities.CarsMoviesEntity;
+import java.util.List;
+
+@Service
+public class CarsMoviesService{
+
+    @Autowired
+    private CarsMoviesRepository carsMoviesRepository;
+
+    public List<CarsMoviesEntity> getAllMovies(){
+        return carsMoviesRepository.findAll();
+    }
+}
